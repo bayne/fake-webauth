4eeb4c70acd17e2bdda113cf672c92b7623de56c
bpayne <bpayne@uci.edu>
2014-08-21 09:28:50 -0700

    Adds campus_id to response by doing an ldap query

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $app->get('/logout', function(Request $request) {
     $response = new RedirectResponse($return_url);
     $expires = new \DateTime();
     $expires->add(new \DateInterval('PT1H'));
-    $response->headers->setCookie(new Cookie('ucinetid_auth', 'no_key', $expires, '/', '.uci.edu'));
+    $response->headers->setCookie(new Cookie('ucinetid_auth', 'no_key', $expires, '/', '.uci.edu', false, false));
 
     return $response;
 });
@@ -32,19 +32,25 @@ $app->match('/login', function(Request $request) use ($app) {
         $expires = new \DateTime();
         $expires->add(new \DateInterval('PT1H'));
 
-        $response->headers->setCookie(new Cookie('ucinetid_auth', $ucinetid, $expires, '/', '.uci.edu'));
+        $response->headers->setCookie(new Cookie('ucinetid_auth', $ucinetid, $expires, '/', '.uci.edu', false, false));
 
         return $response;
     } else {
+
         return $app['twig']->render('login.html.twig');
     }
 });
 $app->get('/check', function(Request $request) use ($app) {
     $ucinetid_auth = $request->query->get('ucinetid_auth');
+    $ds = ldap_connect('ldap.service.uci.edu');
+    $r = ldap_bind($ds);
+    $sr = ldap_search($ds, 'ou=University of California Irvine,o=University of California, c=US', 'ucinetid='.$ucinetid_auth, array('campusid'));
+
     $template_vars = array(
         'ucinetid' => $ucinetid_auth,
         'current_time' => time(),
-        'auth_host' => $request->server->get('REMOTE_ADDR')
+        'auth_host' => $request->server->get('REMOTE_ADDR'),
+        'campus_id' => ldap_get_entries($ds, $sr)[0]['campusid'][0],
     );
 
     return $app['twig']->render('index.html.twig', $template_vars);
